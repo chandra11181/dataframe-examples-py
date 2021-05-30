c66519b9e56e146dde81f2dcf1a692ab1d409cf6
Chandra sekhar Reddy <chandra11181@gmail.com>
2021-05-30 17:26:49 +0530

    My Practice

--- RddC/c_scholaship_recipient_join_filter.py
+++ RddC/c_scholaship_recipient_join_filter.py
@@ -26,18 +26,22 @@ if __name__ == '__main__':
     hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
     hadoop_conf.set("fs.s3a.access.key", app_sec["s3_conf"]["access_key"])
     hadoop_conf.set("fs.s3a.access.key", app_sec["s3_conf"]["secret_access_key"])
-    demographics_rdd = spark.sparkContext.textFile("s3a://"+app_conf["s3_conf"]["s3_bucket"]+ "/demographic.csv")
-    finances_rdd = spark.sparkContext.textFile("s3a://" + app_conf["s3_conf"]["s3_bucket"]+ "/finances.csv")
+    demographics_rdd = spark.sparkContext.textFile("s3a://"+app_conf["s3_conf"]["s3_bucket"] + "/demographic.csv")
+    finances_rdd = spark.sparkContext.textFile("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/finances.csv")
+
     demographics_pair_rdd = demographics_rdd \
         .map(lambda line: line.split(",")) \
+        .map(lambda lst: (
+    int(lst[0]), (int(lst[1]), strtobool(lst[2]), lst[3], lst[4], strtobool(lst[5]), strtobool(lst[6]), int(lst[7]))))
+
+    finances_pair_rdd = finances_rdd \
+        .map(lambda line: line.split(",")) \
         .map(lambda lst: (int(lst[0]), (strtobool(lst[1]), strtobool(lst[2]), strtobool(lst[3]), int(lst[4]))))
-    finance_rdd_pair = finances_rdd \
-        .map(lambda rec: rec.split(",")) \
-        .map(lambda lst: (int(lst[0]), (strtobool(lst[1]), strtobool(lst[2]), strtobool(lst[3]), int(lst[4]))))
+
     print('Participants belongs to \'Switzerland\', having debts and financial dependents,')
     join_pair_rdd = demographics_pair_rdd \
-        .join(finance_rdd_pair) \
-        .filter(lambda rec: (rec[1][0][2] == "Switzerland") and (rec[1][1][0] == 1) and (rec[1][1][1] == 1))
+        .join(finances_pair_rdd) \
+        .filter(lambda rec: (rec[1][0][2] == "Switzerland") and (rec[1][1][0] == 1) and (rec[1][1][1] == 1)) \
 
     join_pair_rdd.foreach(print)
 
